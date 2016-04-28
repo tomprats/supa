2bc39bf1552fb926791027605d4575171c56eff3
Tom Prats <tprats108@gmail.com>
2016-04-28 19:16:57 -0400

    Added Password Reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Supa::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: "www.stewartstownupa.com" }
 end
