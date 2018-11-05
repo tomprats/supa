e726abb6bad5f2acdda4ccd0ed4204c49f3281bf
Tom Prats <tprats108@gmail.com>
2018-11-04 21:31:19 -0500

    Updated emailer to send to not me

--- app/jobs/email_job.rb
+++ app/jobs/email_job.rb
@@ -1,10 +1,8 @@
 class EmailJob < ApplicationJob
   def perform(email_id, preview: true)
     users = preview ? User.super : User.all
-    # TODO: Remove
-    tom = User.tom
     users.find_each.with_index do |user, index|
-      EmailMailer.email(tom.id, email_id).deliver_later(wait: (index * 10).seconds + 1.minute)
+      EmailMailer.email(user.id, email_id).deliver_later(wait: (index * 10).seconds + 1.minute)
     end
   end
 end
